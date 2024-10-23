8eb3332916e36d09ae385949ae98a13af42e6f26
Héctor Martínez <103188721+hectorzin@users.noreply.github.com>
2024-10-23 11:33:57 +0200

    feat: Home Assistant Addon configuration for "support creators work"

--- ha-addon/json2yaml.py
+++ ha-addon/json2yaml.py
@@ -6,7 +6,7 @@ json_file = "/data/options.json"
 yaml_file = "/botaffiumeiro/data/config.yaml"
 
 # Load the JSON file
-with open(json_file, 'r') as f:
+with open(json_file, "r") as f:
     data = json.load(f)
 
 # Convert JSON into the desired YAML structure
@@ -14,40 +14,51 @@ config = {
     "telegram": {
         "bot_token": data.get("bot_token", ""),
         "delete_messages": data.get("delete_messages", True),
-        "excluded_users": [user.get("id") for user in data.get("excluded_users", [])]
+        "excluded_users": [user.get("id") for user in data.get("excluded_users", [])],
     },
     "messages": {
-        "affiliate_link_modified": data.get("msg_affiliate_link_modified", "Here is the modified link with our affiliate program:"),
-        "reply_provided_by_user": data.get("msg_reply_provided_by_user", "Reply provided by")
+        "affiliate_link_modified": data.get(
+            "msg_affiliate_link_modified",
+            "Here is the modified link with our affiliate program:",
+        ),
+        "reply_provided_by_user": data.get(
+            "msg_reply_provided_by_user", "Reply provided by"
+        ),
     },
     "amazon": {
         "advertisers": {
-            advertiser["domain"]: advertiser["id"] for advertiser in data.get("amazon", [])
+            advertiser["domain"]: advertiser["id"]
+            for advertiser in data.get("amazon", [])
         }
     },
     "awin": {
         "publisher_id": data.get("awin_publisher_id", ""),
         "advertisers": {
-            advertiser["domain"]: advertiser["id"] for advertiser in data.get("awin_adversiters", [])
-        }
+            advertiser["domain"]: advertiser["id"]
+            for advertiser in data.get("awin_adversiters", [])
+        },
     },
     "admitad": {
         "publisher_id": data.get("admitad_publisher_id", ""),
         "advertisers": {
-            advertiser["domain"]: advertiser["id"] for advertiser in data.get("admitad_adversiters", [])
-        }
+            advertiser["domain"]: advertiser["id"]
+            for advertiser in data.get("admitad_adversiters", [])
+        },
     },
     "aliexpress": {
         "app_key": data.get("aliexpress_app_key", ""),
         "app_secret": data.get("aliexpress_app_secret", ""),
         "tracking_id": data.get("aliexpress_tracking_id", ""),
-        "discount_codes": "\n".join([code.get("line", "") for code in data.get("aliexpress_discount_codes", [])])
+        "discount_codes": "\n".join(
+            [code.get("line", "") for code in data.get("aliexpress_discount_codes", [])]
+        ),
     },
-    "log_level": "INFO"
+    "log_level": data.get("log_level", "INFO"),
+    "creator_affiliate_percentage": data.get("creator_affiliate_percentage", 10),
 }
 
 # Save the YAML file
-with open(yaml_file, 'w') as f:
+with open(yaml_file, "w") as f:
     yaml.dump(config, f, allow_unicode=True, sort_keys=False)
 
 print(f"YAML file saved to {yaml_file}")
