36377963d738c2ac4ac50dd99ad7ca1756f8471b
Héctor Martínez <103188721+hectorzin@users.noreply.github.com>
2024-10-25 07:10:45 +0200

    feat: Removing old Amazon handler

--- handlers/amazon_handler.py
+++ handlers/amazon_handler.py
@@ -13,48 +13,6 @@ class AmazonHandler(BaseHandler):
     def __init__(self):
         super().__init__()
 
-    async def handle_links2(self, context) -> bool:
-        """Handles Amazon links in the message."""
-        message, modified_text, self.selected_users = self._unpack_context(context)
-        amazon_affiliate_id = self.selected_users.get("amazon", {}).get(
-            "affiliate_id", ""
-        )
-        if not amazon_affiliate_id:
-            self.logger.info("Amazon affiliate ID is not set. Skipping processing.")
-            return False
-
-        self.logger.info(
-            f"{message.message_id}: Handling Amazon links in the message..."
-        )
-        amazon_links = re.findall(AMAZON_PATTERN, modified_text)
-
-        if amazon_links:
-            self.logger.info(
-                f"{message.message_id}: Found {len(amazon_links)} Amazon links. Processing..."
-            )
-            new_text = modified_text
-            for link in amazon_links:
-                # Ensure link is a string, extract from tuple if needed
-                if isinstance(link, tuple):
-                    link = link[0]
-
-                selected_affiliate_data = self.selected_users.get("amazon", {})
-                affiliate_id = selected_affiliate_data.get("affiliate_id", None)
-                affiliate_link = self._generate_affiliate_url(
-                    link,
-                    format_template="{domain}{path_before_query}?{affiliate_tag}={affiliate_id}",
-                    affiliate_tag="tag",
-                    affiliate_id=affiliate_id,
-                )
-                new_text = new_text.replace(link, affiliate_link)
-
-            await self._process_message(message, new_text)
-
-            return True
-
-        self.logger.info(f"{message.message_id}: No Amazon links found in the message.")
-        return False
-
     async def handle_links(self, context) -> bool:
         """Handles Admitad-managed store links in the message."""
 
